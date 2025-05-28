bdccbd33b5780d2c75965899d125a9f2d168ecfe
sbx_user1051 <sbx_user1051@169.254.23.153>
2025-05-28 22:20:56 +0000

    Wed May 28 22:20:56 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,13 +3,27 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        print("skus: ", skus)
+
+        # skus: e.g. "AABBA"
         totalCheckout = 0
-        for i in skus:
-            totalCheckout += 1
+
+        ItemPriceMapping = { 
+            "A": 50,
+            "B": 30,
+            "C": 20,
+            "D": 15,
+        }
+
+        for item in skus:
+            if item not in ItemPriceMapping:
+                return -1
+            
+            totalCheckout += ItemPriceMapping[item]
+            
         return totalCheckout
 
 
         
+
 
 
